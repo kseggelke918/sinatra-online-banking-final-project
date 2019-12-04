f3eb3666ecfcc2d8e5533855dae87f4f120696bb
Katie Seggelke <katie.seggelke@gmail.com>
2019-12-04 16:59:38 +0000

    testing application controller setup, failures

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < Sinatra::Base 
-  
-  get '/' do 
-    "Hello World"
+  configure do 
+    set :views, 'app/views'
   end 
   
+  get '/' do 
+    <h1>Hello World</h1>
+  end 
+  
+  get '/failure' do 
+    erb :failure
+  end 
   
   
 end 
