49a3ab8f4abf0e5ea2d04a7e3d7d9dde20af8245
Katie Seggelke <katie.seggelke@gmail.com>
2019-12-04 21:52:12 +0000

    added full_name column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_211437) do
+ActiveRecord::Schema.define(version: 2019_12_04_215107) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "account_number"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_12_04_211437) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "full_name"
   end
 
 end
