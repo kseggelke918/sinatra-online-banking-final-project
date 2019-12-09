e2dead1cf60aae387739dbb77feea0a5c4fd3e3c
Katie Seggelke <katie.seggelke@gmail.com>
2019-12-09 21:20:36 +0000

    testing all routes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,51 +1,65 @@
-class AccountsController < ApplicationController 
- get '/accounts/accounts' do
-   if logged_in?
-    @user = User.find_by_id(params[:id])
-    @accounts = @user.accounts 
-    erb :'/accounts/accounts'  
-  else 
-    redirect to '/login'
-  end 
- end 
- 
- get '/accounts/new' do
-   if logged_in?
-    erb :'accounts/new'
-  else 
-    redirect to '/login'
-  end 
- end 
- 
- post '/accounts' do 
-  if logged_in?
-    @account = Account.new(account_type: params[:account_type], balance: params[:balance], user_id: params[:user_id], account_number: rand(111111..999999))
-    @account.save 
-    redirect to "accounts/#{@account.id}"
-  else
-    redirect to '/login'
-  end 
- end 
-
-  get '/accounts/:id' do
-    if logged_in?
-      @user = User.find_by_id(params[:id])
-      @accounts = @user.accounts 
-      erb :'accounts/accounts' 
-    else 
-      redirect to '/login'
-    end 
-  end 
-  
-  delete '/accounts/:id/delete' do 
-    if logged_in?
-      @account = Account.find_by_id(params[:id])
-      @account.delete if @account && @account.user == current_user 
-      redirect to '/accounts/accounts'
-    else 
-      redirect to '/login'
-    end 
-  end 
+class AccountsController < ApplicationController
   
   
 end 
+
+
+
+
+
+
+
+
+
+# class AccountsController < ApplicationController 
+# get '/accounts/accounts' do
+#   if logged_in?
+#     @user = User.find_by_id(params[:id])
+#     @accounts = @user.accounts 
+#     erb :'/accounts/accounts'  
+#   else 
+#     redirect to '/login'
+#   end 
+# end 
+ 
+# get '/accounts/new' do
+#   if logged_in?
+#     erb :'accounts/new'
+#   else 
+#     redirect to '/login'
+#   end 
+# end 
+ 
+# post '/accounts' do 
+#   if logged_in?
+#     @account = Account.new(account_type: params[:account_type], balance: params[:balance], user_id: params[:user_id], account_number: rand(111111..999999))
+#     @account.save 
+#     redirect to "accounts/#{@account.id}"
+#   else
+#     redirect to '/login'
+#   end 
+# end 
+
+#   get '/accounts/:id' do
+#     if logged_in?
+#       @user = User.find_by_id(params[:id])
+#       @accounts = @user.accounts 
+#       binding.pry 
+#       erb :'accounts/accounts' 
+#     else 
+#       redirect to '/login'
+#     end 
+#   end 
+  
+#   delete '/accounts/:id/delete' do 
+#     if logged_in?
+#       @account = Account.find_by_id(params[:id])
+#       @account.delete if @account && @account.user == current_user 
+#       redirect to '/accounts/accounts'
+#     else 
+#       redirect to '/login'
+#     end 
+#   end 
+  
+  
+# end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,59 +1,70 @@
 # frozen_string_literal: true 
 require 'pry'
-class UsersController < ApplicationController 
+
+
+class UsersController < ApplicationController
+  
+  
+end 
+
+
+
+
+
+# class UsersController < ApplicationController 
  
- get '/login' do 
-   erb :'users/login' 
- end 
+# get '/login' do 
+#   erb :'users/login' 
+# end 
  
- post '/login' do 
-   user = User.find_by(username: params[:username])
+# post '/login' do 
+#   user = User.find_by(username: params[:username])
    
-   if user && user.authenticate(params[:password])
-    session[:user_id] = user.id 
-    redirect '/accounts/accounts'
-   else 
-    @errors = ["Invalid Username or Password"]
-    erb :failure  
-   end 
+#   if user && user.authenticate(params[:password])
+#     session[:user_id] = user.id 
+#     redirect '/accounts/accounts'
+#   else 
+#     @errors = ["Invalid Username or Password"]
+#     erb :failure  
+#   end 
    
- end 
+# end 
  
- get '/signup' do 
-   erb :'users/signup'
- end 
+# get '/signup' do 
+#   erb :'users/signup'
+# end 
  
- post '/signup' do 
-   # ToDo: tweek conditional statement so that if a user is already found, they are brought into the login page
-   if params[:password] == params[:confirm_password]
-    @user = User.new(full_name: params[:name], username: params[:username], password: params[:password])
-    @user.save 
-    session[:user_id] = @user.id 
-    redirect to "/accounts/new"
-  else 
-    @errors = ["Passwords do not match"]
-    erb :failure
-  end
- end 
+# post '/signup' do 
+#   # ToDo: tweek conditional statement so that if a user is already found, they are brought into the login page
+#   if params[:password] == params[:confirm_password]
+#     @user = User.new(full_name: params[:name], username: params[:username], password: params[:password])
+#     @user.save 
+#     session[:user_id] = @user.id 
+#     redirect to "/accounts/new"
+#   else 
+#     @errors = ["Passwords do not match"]
+#     erb :failure
+#   end
+# end 
  
- get 'users/:id/edit' do 
-  @user = User.find_by_id(params[:id])
-  erb :edit 
- end 
+# get 'users/:id/edit' do 
+#   @user = User.find_by_id(params[:id])
+#   erb :edit 
+# end 
  
- patch 'users/:id' do 
-   @user=User.find_by_id(params[:id])
-   @user.username = params[:username]
-   @user.full_name = params[:full_name]
-   @user.save 
-   redirect to "/users/#{user.id}"
- end 
+# patch 'users/:id' do 
+#   @user=User.find_by_id(params[:id])
+#   @user.username = params[:username]
+#   @user.full_name = params[:full_name]
+#   @user.save 
+#   redirect to "/users/#{user.id}"
+# end 
  
- get '/logout' do 
-   session.clear 
-   redirect '/'
- end 
+# get '/logout' do 
+#   session.clear 
+#   redirect '/'
+# end 
  
     
   
-end 
+# end 
