0b981569cf2fbe9c18a2304665580236fe35146c
Katie Seggelke <katie.seggelke@gmail.com>
2019-12-09 20:47:59 +0000

    added routes in users and accounts for edit and delete

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,29 +1,47 @@
 class AccountsController < ApplicationController 
  get '/accounts/accounts' do
-   @user = User.find_by_id(params[:id])
-   @accounts = @user.accounts 
-  erb :'/accounts/accounts'    
+   if logged_in?
+    @user = User.find_by_id(params[:id])
+    @accounts = @user.accounts 
+    erb :'/accounts/accounts'  
+  else 
+    redirect to '/login'
+  end 
  end 
  
  get '/accounts/new' do
-  erb :'accounts/new' 
+   if logged_in?
+    erb :'accounts/new'
+  else 
+    redirect to '/login'
+  end 
  end 
  
- post '/accounts/new' do 
-  binding.pry 
+ post '/accounts' do 
   @account = Account.new(account_type: params[:account_type], balance: params[:balance], user_id: params[:user_id], account_number: rand(111111..999999))
   @account.save 
   redirect to "accounts/accounts"
  end 
 
   get '/accounts/:id' do
-   @user = User.find_by_id(params[:id])
-   @accounts = @user.accounts 
-   erb :accounts 
+    if logged_in?
+      @user = User.find_by_id(params[:id])
+      @accounts = @user.accounts 
+      erb :accounts 
+    else 
+      redirect to '/login'
+    end 
   end 
   
+  delete '/accounts/:id/delete' do 
+    if logged_in?
+      @account = Account.find_by_id(params[:id])
+      @account.delete if @account && @account.user == current_user 
+      redirect to '/accounts/accounts'
+    else 
+      redirect to '/login'
+    end 
+  end 
   
- 
-
   
 end 
