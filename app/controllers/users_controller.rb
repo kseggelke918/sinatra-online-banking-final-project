aa0dd38313295675c6f61c70d359d74a6c4b0466
Katie Seggelke <katie.seggelke@gmail.com>
2019-12-10 19:41:53 +0000

    fixed display path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   
   get '/:id/accounts' do 
     @user = User.find_by_id(params[:id])
-    @accounts = Account.find_by(user_id: params[:id])
+    @accounts = @user.accounts 
     erb :'accounts/accounts'
   end 
   
