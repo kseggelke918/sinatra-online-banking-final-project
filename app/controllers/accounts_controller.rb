53b116585e8a48ce7b324865f1b9dad33dea9e67
Katie Seggelke <katie.seggelke@gmail.com>
2019-12-11 21:40:28 +0000

    adding requirement to select account type when opening new acct

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -7,12 +7,18 @@ class AccountsController < ApplicationController
  
  post '/accounts' do 
   if logged_in?
-    @user = current_user
-    @account = Account.new(account_type: params[:account_type], balance: params[:balance], user_id: @user.id, account_number: rand(111111..999999))
-    @account.save 
-    redirect to "/#{@user.id}/accounts"
-  else
-    redirect to '/login'
+    if params[:account_type] != ""
+      @user = current_user
+      @account = Account.new(account_type: params[:account_type], balance: params[:balance], user_id: @user.id, account_number: rand(111111..999999))
+      @account.save 
+      redirect to "/#{@user.id}/accounts"
+    else 
+      @errors = ["Please select an account type"]
+      erb :failure
+      redirect to "/accounts/new"
+    end 
+    else
+      redirect to '/login'
   end    
  end 
  
